13c1ebfbdf7ff666cdfe933a844e2763363bd644
oga3999 <zgmf.x09a3999@gmail.com>
2017-08-29 15:25:46 +0000

    sendgrid host変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'https://github.com/oga3999/big_task_3.git' }
+  config.action_mailer.default_url_options = { host: 'http://13.114.116.88/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
